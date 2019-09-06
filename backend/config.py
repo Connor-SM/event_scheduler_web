509c601f64b0cc3f87aabcb298f4c1e8a8ed07f6
Connor Milliken <cmilliken09@gmail.com>
2019-09-06 14:51:08 -0400

    Updated to reflect new version

---
+++ backend/config.py
@@ -0,0 +1,20 @@
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+
+
+    # setting up all email config variables
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'cmilliken09@gmail.com'
+    MAIL_PASSWORD = 'qobzfsjrhebtfxqy'
+    ADMINS = ['cmilliken09@gmail.com']
---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header';
+import Events from './views/events';
+import Schedule from './views/schedule';
+import { Switch, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path='/' render={() => <Schedule />} />
+        <Route exact path='/events' render={() => <Events />} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
